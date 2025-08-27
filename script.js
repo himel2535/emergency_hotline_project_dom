c4d54105d655c0d30c32b53e952968893f11a397
himel2535 <monwarhossanhimel@gmail.com>
2025-08-28 02:49:28 +0600

    clicking of call section is done except call history

--- script.js
+++ script.js
@@ -31,23 +31,45 @@ heartClick("heart-btn8");
 heartClick("heart-btn9");
 
 // -----call------
+function forCall(id, id1, id2) {
+  document.getElementById(id).addEventListener("click", function () {
+    let coin = parseInt(document.getElementById("coin-number").innerText);
+    updatedCoin = coin - 20;
 
-document.getElementById("call-btn1").addEventListener("click", function () {
-  let coin = parseInt(document.getElementById("coin-number").innerText);
-  updatedCoin = coin - 20;
+    if (updatedCoin < 20) {
+      alert("Insufficient Coin");
+      return;
+    }
 
-  if (updatedCoin < 20) {
-    alert("Insufficient Coin");
-    return;
-  }
+    document.getElementById("coin-number").innerText = updatedCoin;
 
-  document.getElementById("coin-number").innerText = updatedCoin;
+    alerted(id1, id2);
+  });
+}
 
-  alerted("national-emergency-number","national-emergency-number-call")
+forCall(
+  "call-btn1",
+  "national-emergency-number",
+  "national-emergency-number-call"
+);
+forCall("call-btn2", "police-helpline-number", "police-helpline-number-call");
+forCall("call-btn3", "fire-service-number", "fire-service-number-call");
+forCall("call-btn4", "ambulance-service", "ambulance-service-call");
+forCall("call-btn5", "women&child-helpline", "women&child-helpline-call");
+forCall(
+  "call-btn6",
+  "anti-corruption-helpline",
+  "anti-corruption-helpline-call"
+);
+forCall("call-btn7", "electricity-helpline", "electricity-helpline-call");
+forCall("call-btn8", "brac-helpline", "brac-helpline-call");
+forCall(
+  "call-btn9",
+  "bangladesh-railway-helpline",
+  "bangladesh-railway-helpline-call"
+);
 
-});
-
-function alerted(id1,id2) {
+function alerted(id1, id2) {
   const forAlert1 = document.getElementById(id1).innerText;
   const forAlert2 = document.getElementById(id2).innerText;
   alert(forAlert1 + " is : " + forAlert2);
