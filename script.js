156f735ff0cb501f866bd2c324e57602a5593884
himel2535 <monwarhossanhimel@gmail.com>
2025-08-28 00:14:39 +0600

    after one click heart will red and count increase one and then after click two heart will minimizes color and count will be minus  overall this heart section is completed

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let clicked = {};
 function heartClick(id) {
-  document.getElementById(id).addEventListener("click", function () {
+  const btn = document.getElementById(id);
+  btn.addEventListener("click", function () {
     if (!clicked[id]) {
       let heart = parseInt(document.getElementById("heart-number").innerText);
       updatedHeart = heart + 1;
       document.getElementById("heart-number").innerText = updatedHeart;
+      btn.style.color = "red";
       clicked[id] = true;
+    } else {
+      let heart = parseInt(document.getElementById("heart-number").innerText);
+      updatedHeart = heart - 1;
+      document.getElementById("heart-number").innerText = updatedHeart;
+      btn.style.color = "";
+      clicked[id] = false;
     }
   });
 }
@@ -19,7 +27,3 @@ heartClick("heart-btn6");
 heartClick("heart-btn7");
 heartClick("heart-btn8");
 heartClick("heart-btn9");
-
-
-
-
